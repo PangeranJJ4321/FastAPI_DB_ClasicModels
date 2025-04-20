bef396aa9264c8354d5ba16fb731eacaae47b633
PangeranJJ4321 <pangeranjuhrifar@gmail.com>
2025-04-20 18:51:50 +0800

    Solved  ACCESS_TOKEN_EXPIRE_MINUTES must be int

--- auth/auth.py
+++ auth/auth.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 # Password context untuk hashing dan verifikasi
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
